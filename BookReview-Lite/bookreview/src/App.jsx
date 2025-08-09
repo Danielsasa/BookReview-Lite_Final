396b0892e8ba8c2efd418057574fed6c0542d26b
Danielsasa <danielsasa1@hotmail.com>
2025-08-09 13:30:39 -0600

    correccion de errores

--- BookReview-Lite/bookreview/src/App.jsx
+++ BookReview-Lite/bookreview/src/App.jsx
@@ -1,5 +1,6 @@
 
 import React, { useState, createContext } from 'react';
+import { UserProvider } from './context/UserContext';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import BookDetail from './pages/BookDetail';
@@ -21,22 +22,24 @@ function App() {
   }, [darkMode]);
 
   return (
-    <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
-      <Router>
-        <Header />
-        <main className={darkMode ? 'dark' : ''} style={{ minHeight: '80vh', padding: '2rem', background: darkMode ? '#181a1b' : '#f6f8fa', color: darkMode ? '#fff' : '#222', transition: 'background .2s, color .2s' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/book/:id" element={<BookDetail />} />
-            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/add-book" element={<AddBook />} />
-          </Routes>
-        </main>
-        <Footer />
-      </Router>
-    </ThemeContext.Provider>
+    <UserProvider>
+      <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+        <Router>
+          <Header />
+          <main className={darkMode ? 'dark' : ''} style={{ minHeight: '80vh', padding: '2rem', background: darkMode ? '#181a1b' : '#f6f8fa', color: darkMode ? '#fff' : '#222', transition: 'background .2s, color .2s' }}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/book/:id" element={<BookDetail />} />
+              <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/add-book" element={<AddBook />} />
+            </Routes>
+          </main>
+          <Footer />
+        </Router>
+      </ThemeContext.Provider>
+    </UserProvider>
   );
 }
 
